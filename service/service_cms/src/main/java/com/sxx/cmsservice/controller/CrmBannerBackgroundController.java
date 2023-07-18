62adbdee367107ca7b53ae32fd2daa0dba7f9705
SxxStar <2291874995@qq.com>
2023-07-18 19:31:52 +0800

    cms模块初始化

---
+++ service/service_cms/src/main/java/com/sxx/cmsservice/controller/CrmBannerBackgroundController.java
@@ -0,0 +1,20 @@
+package com.sxx.cmsservice.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author SxxStar
+ * @description 后台轮播图管理接口
+ * @since 2023-07-18
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/cmsservice/bannerbackground")
+public class CrmBannerBackgroundController {
+
+}
+
--- service/service_cms/src/main/java/com/sxx/cmsservice/controller/CrmBannerReceptionController.java
+++ service/service_cms/src/main/java/com/sxx/cmsservice/controller/CrmBannerReceptionController.java
@@ -3,21 +3,17 @@ package com.sxx.cmsservice.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
- * 首页banner表 前端控制器
- * </p>
- *
  * @author SxxStar
+ * @description 前台轮播图接口
  * @since 2023-07-18
  */
 @CrossOrigin
 @RestController
-@RequestMapping("/cmsservice/crm-banner")
-public class CrmBannerController {
+@RequestMapping("/cmsservice/reception")
+public class CrmBannerReceptionController {
 
 }
 
